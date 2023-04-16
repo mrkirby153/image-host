12b6ba5226079baefe080b66de65814728861376
mrkirby153 <mr.austinwhyte@gmail.com>
2023-04-15 20:25:30 -0700

    Add code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,88 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cache } from 'hono/cache';
+import { basicAuth } from 'hono/basic-auth';
+import { BodyData } from 'hono/utils/body';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+type Bindings = {
+	BUCKET: R2Bucket,
+	USERNAME: string,
+	PASSWORD: string
 }
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+const CACHE_DURATION = 60 * 60 * 24;
+const FILENAME_LENGTH = 8;
+
+
+function randomFileName() {
+	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	let result = '';
+	for (let i = 0; i < FILENAME_LENGTH; i++) {
+		result += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return result;
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get(
+	'*',
+	cache({
+		cacheName: 'r2-image-worker'
+	})
+)
+
+app.post('/_upload', async (c, next) => {
+	const auth = basicAuth({
+		username: c.env.USERNAME, password: c.env.PASSWORD
+	})
+	await auth(c, next)
+})
+
+app.post('/_upload', async (c, next) => {
+	const body = await c.req.parseBody();
+
+	const maybeFile = body.file
+	if (!maybeFile || !(maybeFile instanceof File)) return c.body('Missing file', 400);
+
+
+	const getFileName = async () => {
+		if (body.name) {
+			return body.name
+		}
+		let attempts = 0;
+		while (attempts < 10) {
+			const fileName = randomFileName();
+			const object = await c.env.BUCKET.head(fileName);
+			if (!object) return fileName;
+			attempts++;
+		}
+	}
+
+	const parts = maybeFile.name.split('.');
+	const extension = parts[parts.length - 1];
+
+	const fileName = await getFileName();
+	const withExtension = `${fileName}.${extension}`;
+	const file = await maybeFile.arrayBuffer();
+
+	await c.env.BUCKET.put(withExtension, file, { httpMetadata: { contentType: maybeFile.type } });
+
+	const baseUrl = new URL(c.req.url).origin;
+
+	return c.body(`${baseUrl}/${withExtension}`);
+})
+
+app.get('/:key', async (c) => {
+	const key = c.req.param('key');
+
+	const object = await c.env.BUCKET.get(key);
+	if (!object) return c.notFound();
+	const data = await object.arrayBuffer();
+	const contentType = object.httpMetadata?.contentType || '';
+	return c.body(data, 200, {
+		'Cache-Control': `public max- age=${CACHE_DURATION}`,
+		'Content-Type': contentType
+	})
+})
+
+export default app
