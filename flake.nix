b817693d7e084e5cbc06a180907e655358de50ca
mrkirby153 <mr.austinwhyte@gmail.com>
2024-11-02 10:55:31 -0700

    Allow inline mp4s

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "An image host";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [wrangler];
+      };
+      formatter = pkgs.alejandra;
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,110 @@
-import { Hono } from 'hono';
-import { cache } from 'hono/cache';
-import { basicAuth } from 'hono/basic-auth';
-import { BodyData } from 'hono/utils/body';
+import { Hono } from "hono";
+import { cache } from "hono/cache";
+import { basicAuth } from "hono/basic-auth";
 
 type Bindings = {
-	BUCKET: R2Bucket,
-	USERNAME: string,
-	PASSWORD: string
-}
+  BUCKET: R2Bucket;
+  USERNAME: string;
+  PASSWORD: string;
+};
 
 const CACHE_DURATION = 60 * 60 * 24;
 const FILENAME_LENGTH = 8;
 
-
 function randomFileName() {
-	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-	let result = '';
-	for (let i = 0; i < FILENAME_LENGTH; i++) {
-		result += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return result;
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+  for (let i = 0; i < FILENAME_LENGTH; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
 }
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get(
-	'*',
-	cache({
-		cacheName: 'r2-image-worker'
-	})
-)
+  "*",
+  cache({
+    cacheName: "r2-image-worker",
+  })
+);
 
-app.post('/_upload', async (c, next) => {
-	const auth = basicAuth({
-		username: c.env.USERNAME, password: c.env.PASSWORD
-	})
-	await auth(c, next)
-})
-
-app.post('/_upload', async (c, next) => {
-	const body = await c.req.parseBody();
-
-	const maybeFile = body.file
-	if (!maybeFile || !(maybeFile instanceof File)) return c.body('Missing file', 400);
-
-
-	const getFileName = async () => {
-		if (body.name) {
-			return body.name
-		}
-		let attempts = 0;
-		while (attempts < 10) {
-			const fileName = randomFileName();
-			const object = await c.env.BUCKET.head(fileName);
-			if (!object) return fileName;
-			attempts++;
-		}
-	}
-
-	const parts = maybeFile.name.split('.');
-	const extension = parts[parts.length - 1];
-
-	const fileName = await getFileName();
-	const withExtension = `${fileName}.${extension}`;
-	const file = await maybeFile.arrayBuffer();
-
-	await c.env.BUCKET.put(withExtension, file, { httpMetadata: { contentType: maybeFile.type } });
-
-	const baseUrl = new URL(c.req.url).origin;
-
-	return c.body(`${baseUrl}/${withExtension}`);
-})
-
-app.get('/:key', async (c) => {
-	const key = c.req.param('key');
-
-	const object = await c.env.BUCKET.get(key);
-	if (!object) return c.notFound();
-	const data = await object.arrayBuffer();
-	const contentType = object.httpMetadata?.contentType || '';
-	return c.body(data, 200, {
-		'Cache-Control': `public max- age=${CACHE_DURATION}`,
-		'Content-Type': contentType
-	})
-})
-
-app.delete('/:key', async (c, next) => {
-	const auth = basicAuth({
-		username: c.env.USERNAME, password: c.env.PASSWORD
-	})
-	await auth(c, next)
-})
-
-app.delete('/:key', async (c) => {
-	const key = c.req.param('key');
-	await c.env.BUCKET.delete(key);
-	return c.body('OK');
+app.post("/_upload", async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.USERNAME,
+    password: c.env.PASSWORD,
+  });
+  await auth(c, next);
 });
 
-export default app
+app.post("/_upload", async (c, next) => {
+  const body = await c.req.parseBody();
+
+  const maybeFile = body.file;
+  if (!maybeFile || !(maybeFile instanceof File))
+    return c.body("Missing file", 400);
+
+  const getFileName = async () => {
+    if (body.name) {
+      return body.name;
+    }
+    let attempts = 0;
+    while (attempts < 10) {
+      const fileName = randomFileName();
+      const object = await c.env.BUCKET.head(fileName);
+      if (!object) return fileName;
+      attempts++;
+    }
+  };
+
+  const parts = maybeFile.name.split(".");
+  const extension = parts[parts.length - 1];
+
+  const fileName = await getFileName();
+  const withExtension = `${fileName}.${extension}`;
+  const file = await maybeFile.arrayBuffer();
+
+  await c.env.BUCKET.put(withExtension, file, {
+    httpMetadata: { contentType: maybeFile.type },
+  });
+
+  const baseUrl = new URL(c.req.url).origin;
+
+  return c.body(`${baseUrl}/${withExtension}`);
+});
+
+app.get("/:key", async (c) => {
+  const key = c.req.param("key");
+
+  const object = await c.env.BUCKET.get(key);
+  console.log(object);
+  if (!object) return c.notFound();
+  const data = await object.arrayBuffer();
+  let contentType = object.httpMetadata?.contentType || "";
+
+  if (contentType === "application/octet-stream" && key.endsWith(".mp4")) {
+    contentType = "video/mp4";
+  }
+
+  return c.body(data, 200, {
+    "Cache-Control": `public max-age=${CACHE_DURATION}`,
+    "Content-Type": contentType,
+  });
+});
+
+app.delete("/:key", async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.USERNAME,
+    password: c.env.PASSWORD,
+  });
+  await auth(c, next);
+});
+
+app.delete("/:key", async (c) => {
+  const key = c.req.param("key");
+  await c.env.BUCKET.delete(key);
+  return c.body("OK");
+});
+
+export default app;
